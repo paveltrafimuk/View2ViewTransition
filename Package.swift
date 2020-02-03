37c89eaf5626053f0051b9d2ce29960e58a47ca9
Pavel Trafimuk <paveltrafimuk@icloud.com>
2020-02-04 00:28:57 +0300

    updated SPM file

--- Package.swift
+++ Package.swift
@@ -1,3 +1,14 @@
 import PackageDescription
 
-let package = Package(name: "View2ViewTransition")
+let package = Package(
+    name: "View2ViewTransition",
+    products: [
+        .library(name: "View2ViewTransition", targets: ["View2ViewTransition"]),
+    ],
+    dependencies: [ ],
+    targets: [
+        .target(
+            name: "View2ViewTransition",
+            dependencies: [])
+    ]
+)
